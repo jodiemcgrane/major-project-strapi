18b63efa7b556bef11b0eff01dfabf134bd6c262
jodiemcgrane <48021738+jodiemcgrane@users.noreply.github.com>
2022-02-22 17:40:37 +0000

    Updated middleware 2

--- config/middleware.js
+++ config/middleware.js
@@ -2,9 +2,9 @@ module.exports = {
   settings: {
     cors: {
       enabled: true,
-      //headers: "*",
+      headers: "*",
       origin: [
-        "http://localhost:8000/app",
+        "http://localhost:8000",
         "http://localhost:1337",
         "https://internport-api.herokuapp.com",
       ],
